d5d3a405d426e4d0d07b148caa650d0539fee509
Symlink GmbH <info@symlink.de>
2019-08-19 00:06:51 +0000

    release
    Signed-off-by: Symlink GmbH <info@symlink.de>

---
+++ src/csv/CSVParser.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2018-2019 Symlink GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
+
+import PapaParse, { ParseResult } from "papaparse";
+
+export class CSVParser {
+  public static parseCSVFromBase64(base64: string, flatten?: boolean): Array<string> | ParseResult {
+    const buffer: Buffer = Buffer.from(base64, "base64");
+    const char: string = buffer.toString("ascii");
+    const parseResult = this.parse(char);
+
+    if (flatten) {
+      return this.flatten(parseResult);
+    }
+    return parseResult;
+  }
+
+  private static parse(text: string): ParseResult {
+    return PapaParse.parse(text);
+  }
+
+  private static flatten(result: ParseResult): Array<string> {
+    const { data } = result;
+    const flattenData: Array<string> = [];
+    data.map((entry) => {
+      entry.map((indexes: string) => {
+        if (indexes.length >= 1) {
+          flattenData.push(indexes);
+        }
+      });
+    });
+    return flattenData;
+  }
+}
---
+++ src/ip/IpParser.ts
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2018-2019 Symlink GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
+
+// tslint:disable-next-line:no-var-requires
+const IP = require("ip-subnet-calculator");
+import { PkParse } from "@symlinkde/eco-os-pk-models";
+
+export class IPParser {
+  public static calculateSubnetMask(address: string): PkParse.ICalculatedSubnetMask | null {
+    if (address.indexOf("/") > -1 && this.isValidIpAddress(address)) {
+      const subnetPrefix: number = Number(address.substring(address.indexOf("/") + 1, address.length));
+      const ipAdr: string = address.substring(0, address.indexOf("/"));
+      return IP.calculateSubnetMask(ipAdr, subnetPrefix);
+    }
+
+    return null;
+  }
+
+  public static isValidIpAddress(address: string): boolean {
+    if (address.indexOf("/") > -1) {
+      const ipAdr: string = address.substring(0, address.indexOf("/"));
+      return IP.isIp(ipAdr);
+    }
+
+    return IP.isIp(address);
+  }
+
+  public static extractIpV4Address(address: string | string[]): string | null {
+    if (address === undefined || address === null || this.isIpLocalhost(address)) {
+      return null;
+    }
+
+    return this.extractIp(address);
+  }
+
+  public static isIpLocalhost(address: string | string[]): boolean {
+    if (address === "::1" || address === "127.0.0.1") {
+      return true;
+    }
+
+    return false;
+  }
+
+  private static extractIp(address: string | string[]): string {
+    if (Array.isArray(address) && address.length > 0) {
+      if (address.includes("::ffff:")) {
+        address = address[0].split(":").reverse()[0];
+      }
+      return address[0].replace(/:\d+$/, "");
+    }
+
+    if (!Array.isArray(address)) {
+      if (address.includes("::ffff:")) {
+        return address
+          .replace(/:\d+$/, "")
+          .split(":")
+          .reverse()[0];
+      }
+      return address.replace(/:\d+$/, "");
+    }
+
+    return "";
+  }
+}
